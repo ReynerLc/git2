815cc99fc5903bfe74b6bf2f3803d082104567ef
Reyner <rlopez_jmr@outlook.com>
2024-12-14 22:14:11 -0500

    El profe es bueno

---
+++ arreglo2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " Hola mundo!! me retaron";
+    int arreglo[] = {1,2,3,4,5};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arreglo[i];
+    }
+    
+    return 0;
+}
